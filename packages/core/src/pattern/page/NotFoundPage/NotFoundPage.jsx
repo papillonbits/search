7237f820d6b487037602863d3d853f072bc2498f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2026-01-07 13:04:47 +0100

    refactor(core/notfound): remove deprecated defaultProps from page

--- packages/core/src/pattern/page/NotFoundPage/NotFoundPage.jsx
+++ packages/core/src/pattern/page/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,9 @@
 import { NotFoundTemplate } from '../../template/NotFoundTemplate'
-import { defaultProps, propTypes } from './NotFoundPage.prop'
+import { propTypes } from './NotFoundPage.prop'
 
 export function NotFoundPage() {
   return <NotFoundTemplate />
 }
-
-NotFoundPage.defaultProps = defaultProps
 
 NotFoundPage.propTypes = propTypes
 
